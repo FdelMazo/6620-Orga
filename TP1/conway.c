e3b82c6b317d4905cbe6495092c137bf851f5598
Maximiliano Yacobucci <ae_myacobucci@idcontractors.net>
2020-05-13 20:21:56 -0300

    Amplia el mensaje de ayuda y las opciones de entrada

--- TP1/conway.c
+++ TP1/conway.c
@@ -3,16 +3,26 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define help_message "Uso:\n  conway -h\n  conway -V\n  conway i M N inputfile [-o outputprefix]\n"\
+                     "Opciones:\n  -h, --help     Imprime este mensaje.\n  "\
+                     "-V, --version  Da la versión del programa.\n  -o Prefijo de los archivos de salida.\n"\
+                     "Ejemplos:\n conway 10 20 20 glider -o estado\n "\
+                     "Representa 10 iteraciones del Juego de la Vida en una matriz de 20x20,\n "\
+                     "con un estado inicial tomado del archivo ‘‘glider’’.\n "\
+                     "Los archivos de salida se llamarán estado_n.pbm.\n "\
+                     "Si no se da un prefijo para los archivos de salida,\n "\
+                     "el prefijo será el nombre del archivo de entrada."
 
 int main(int argc, char *argv[]) {
     int opt = 0;
     char *prefix_arg = NULL;
-    while ((opt = getopt(argc, argv, "hvo:")) != -1) {
+    while ((opt = getopt(argc, argv, "hvVo:-")) != -1) {
         switch (opt) {
         case 'h':
-            printf("Use: conway i M N inputfile [-o outputprefix]\n");
+            printf("%s\n", help_message);
             return 0;
         case 'v':
+        case 'V':
             printf("conway 0.0.1\n");
             return 0;
         case 'o':
