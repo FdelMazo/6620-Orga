7026fa42ebd4c7138ac32354c062816f204e011b
FdelMazo <fdelmazo@fi.uba.ar>
2020-05-23 19:22:43 -0300

    Agrega modo interactivo por terminal

--- TP1/conway.c
+++ TP1/conway.c
@@ -3,20 +3,30 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define help_message "Uso:\n  conway -h\n  conway -V\n  conway i M N inputfile [-o outputprefix]\n"\
-                     "Opciones:\n  -h, --help     Imprime este mensaje.\n  "\
-                     "-V, --version  Da la versión del programa.\n  -o Prefijo de los archivos de salida.\n"\
-                     "Ejemplos:\n conway 10 20 20 glider -o estado\n "\
-                     "Representa 10 iteraciones del Juego de la Vida en una matriz de 20x20,\n "\
-                     "con un estado inicial tomado del archivo ‘‘glider’’.\n "\
-                     "Los archivos de salida se llamarán estado_n.pbm.\n "\
-                     "Si no se da un prefijo para los archivos de salida,\n "\
-                     "el prefijo será el nombre del archivo de entrada."
+
+#define ESC 27
+#define help_message                                                           \
+    "Uso:\n  conway -h\n  conway -V\n  conway i M N inputfile [-o "            \
+    "outputprefix] [-p]\n"                                                     \
+    "Opciones:\n"                                                              \
+    "  -h, --help     Imprime este mensaje.\n"                                 \
+    "  -V, --version  Da la versión del programa.\n"                          \
+    "  -o             Prefijo de los archivos de salida.\n"                    \
+    "  -p             Muestra los estados por terminal.\n"                     \
+    "Ejemplos:\n"                                                              \
+    "  conway 10 20 20 glider -o estado\n"                                     \
+    "   Representa 10 iteraciones del Juego de la Vida en una matriz de "      \
+    "20x20,\n"                                                                 \
+    "   con un estado inicial tomado del archivo ‘‘glider’’.\n"        \
+    "   Los archivos de salida se llamarán estado_n.pbm.\n"                   \
+    "   Si no se da un prefijo para los archivos de salida,\n"                 \
+    "   el prefijo será el nombre del archivo de entrada."
 
 int main(int argc, char *argv[]) {
     int opt = 0;
     char *prefix_arg = NULL;
-    while ((opt = getopt(argc, argv, "hvVo:-")) != -1) {
+    bool interactive = false;
+    while ((opt = getopt(argc, argv, "phvVo:-")) != -1) {
         switch (opt) {
         case 'h':
             printf("%s\n", help_message);
@@ -27,6 +37,10 @@ int main(int argc, char *argv[]) {
             return 0;
         case 'o':
             prefix_arg = optarg;
+            break;
+        case 'p':
+            interactive = true;
+            break;
         }
     }
 
@@ -48,12 +62,23 @@ int main(int argc, char *argv[]) {
     }
 
     printf("Reading initial state\n");
+    if (interactive) {
+        printf("Entering interactive mode:\n Press ENTER to see the next "
+               "state\n Press ESC followed by ENTER to finish execution\n\n");
+        char c = getchar();
+        while (c != ESC) {
+            matrix_print(matrix, stdout);
+            matrix_update(matrix);
+            c = getchar();
+        }
+        return 0;
+    }
+
     for (int i = 0; i < I; i++) {
         if (!matrix_save(matrix, prefix))
             fprintf(stderr, "Error saving state %03d\n", i);
         else
             printf("Saving state %s_%03d.pbm\n", prefix, i);
-        matrix_print(matrix, stdout);
         matrix_update(matrix);
     }
 
