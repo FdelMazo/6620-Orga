c7ab762ba5c7d474cb2d4bc708944739ab2b0b1b
FdelMazo <fdelmazo@fi.uba.ar>
2020-07-06 21:32:40 -0300

    Reformatea el codigo basado en el estilo LLVM

--- TP2/cache.c
+++ TP2/cache.c
@@ -1,7 +1,7 @@
 #include "cache.h"
-#include <string.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 
 #define CACHE_SIZE 4096
 #define BLOCK_SIZE 128
@@ -14,43 +14,34 @@
 #define INDEX_SIZE (log(CACHE_SETS) / log(2))
 #define TAG_SIZE (ADDRESS_SIZE - INDEX_SIZE - OFFSET_SIZE)
 
-
-typedef struct
-{
+typedef struct {
     unsigned char data[MAIN_MEMORY_SIZE];
 } main_memory_t;
 
-
-typedef struct
-{
+typedef struct {
     unsigned int tag;
     int counter;
     bool valid;
     unsigned char data[BLOCK_SIZE];
 } cache_block_t;
 
-
-typedef struct
-{
+typedef struct {
     cache_block_t blocks[CACHE_SETS][N_WAYS];
     int miss_count;
     int hit_count;
 } cache_t;
 
-
 unsigned int _get_tag(unsigned int address);
 void _update_cache_count(bool data_is_in_cache);
 unsigned int _get_block(unsigned int address);
-unsigned char _read_from_cache(unsigned int set, unsigned int way, unsigned int offset);
+unsigned char _read_from_cache(unsigned int set, unsigned int way,
+                               unsigned int offset);
 void _update_blocks_counter_from_set(unsigned int set);
-
 
 main_memory_t main_memory;
 cache_t cache;
 
-
-void init()
-{
+void init() {
     printf("Executing init()\n");
     memset(main_memory.data, 0, MAIN_MEMORY_SIZE);
 
@@ -59,57 +50,44 @@ void init()
     cache.hit_count = 0;
 }
 
-unsigned int get_offset(unsigned int address)
-{
-    return address % BLOCK_SIZE;
-}
+unsigned int get_offset(unsigned int address) { return address % BLOCK_SIZE; }
 
-unsigned int find_set(unsigned int address)
-{
+unsigned int find_set(unsigned int address) {
     return address / BLOCK_SIZE % CACHE_SETS;
 }
 
-float get_miss_rate()
-{
+float get_miss_rate() {
     if (cache.miss_count == 0) return 0;
     int total_count = cache.miss_count + cache.hit_count;
     return cache.miss_count / (float)total_count;
 }
 
-unsigned int select_oldest(unsigned int setnum) 
-{
-    cache_block_t* blocks_in_set = cache.blocks[setnum];
+unsigned int select_oldest(unsigned int setnum) {
+    cache_block_t *blocks_in_set = cache.blocks[setnum];
     cache_block_t oldest_block = blocks_in_set[0];
     unsigned int oldest_block_way = 0;
-    for (size_t i = 1; i < N_WAYS; i++) 
-    {
-        if (oldest_block.counter < blocks_in_set[i].counter) 
-        { 
+    for (size_t i = 1; i < N_WAYS; i++) {
+        if (oldest_block.counter < blocks_in_set[i].counter) {
             oldest_block = blocks_in_set[i];
             oldest_block_way = i;
         }
     }
     return oldest_block_way;
-
 }
 
-int compare_tag(unsigned int tag, unsigned int set) 
-{
-    cache_block_t* blocks_in_set = cache.blocks[set];
+int compare_tag(unsigned int tag, unsigned int set) {
+    cache_block_t *blocks_in_set = cache.blocks[set];
     cache_block_t block;
-    for (int i = 0; i < N_WAYS; i++) 
-    {
+    for (int i = 0; i < N_WAYS; i++) {
         block = blocks_in_set[i];
-        if (block.valid && block.tag == tag) 
-        {
+        if (block.valid && block.tag == tag) {
             return i;
         }
     }
     return -1;
 }
 
-void read_tocache(unsigned int blocknum, unsigned int way, unsigned int set) 
-{
+void read_tocache(unsigned int blocknum, unsigned int way, unsigned int set) {
     int block_starting_address = blocknum * BLOCK_SIZE;
     cache_block_t block = cache.blocks[set][way];
     memcpy(&main_memory.data[block_starting_address], block.data, BLOCK_SIZE);
@@ -118,8 +96,7 @@ void read_tocache(unsigned int blocknum, unsigned int way, unsigned int set)
     block.counter = 0;
 }
 
-unsigned char read_byte(unsigned int address)
-{
+unsigned char read_byte(unsigned int address) {
     unsigned int tag = _get_tag(address);
     unsigned int set = find_set(address);
     int way = compare_tag(tag, set);
@@ -127,8 +104,7 @@ unsigned char read_byte(unsigned int address)
     bool is_in_cache = way >= 0;
 
     _update_blocks_counter_from_set(set);
-    if (is_in_cache) 
-    {
+    if (is_in_cache) {
         unsigned int offset = get_offset(address);
         byte_read = _read_from_cache(set, way, offset);
     } else {
@@ -142,11 +118,10 @@ unsigned char read_byte(unsigned int address)
     return byte_read;
 }
 
-/*Como la cache esta implementada con Write Through y no-write allocate, si escribimos
-en la cache, esto nos garantiza que el bloque correspondiente a la direccion se encuentra
-en la misma. */
-void write_tocache(unsigned int address, unsigned char value) 
-{
+/*Como la cache esta implementada con Write Through y no-write allocate, si
+escribimos en la cache, esto nos garantiza que el bloque correspondiente a la
+direccion se encuentra en la misma. */
+void write_tocache(unsigned int address, unsigned char value) {
     unsigned int tag = _get_tag(address);
     unsigned int set = find_set(address);
     unsigned int offset = get_offset(address);
@@ -156,8 +131,7 @@ void write_tocache(unsigned int address, unsigned char value)
     block.counter = 0;
 }
 
-void write_byte(unsigned int address, unsigned char value)
-{
+void write_byte(unsigned int address, unsigned char value) {
     unsigned int tag = _get_tag(address);
     unsigned int set = find_set(address);
     int way = compare_tag(tag, set);
@@ -165,47 +139,37 @@ void write_byte(unsigned int address, unsigned char value)
 
     _update_blocks_counter_from_set(set);
     main_memory.data[address] = value;
-    if (is_in_cache) 
-    {
+    if (is_in_cache) {
         write_tocache(address, value);
     }
     _update_cache_count(is_in_cache);
 }
 
-
-
 /* Funciones auxiliares */
 
-unsigned char _read_from_cache(unsigned int set, unsigned int way, unsigned int offset) {
+unsigned char _read_from_cache(unsigned int set, unsigned int way,
+                               unsigned int offset) {
     cache_block_t block = cache.blocks[set][way];
     block.counter = 0;
     return block.data[offset];
 }
 
-void _update_blocks_counter_from_set(unsigned int set) 
-{
-    cache_block_t* blocks_in_set = cache.blocks[set];
-    for (size_t i = 0; i < N_WAYS; i++) 
-    {
+void _update_blocks_counter_from_set(unsigned int set) {
+    cache_block_t *blocks_in_set = cache.blocks[set];
+    for (size_t i = 0; i < N_WAYS; i++) {
         blocks_in_set[i].counter += 1;
     }
 }
 
-unsigned int _get_block(unsigned int address) 
-{
-    return address / BLOCK_SIZE;
-}
+unsigned int _get_block(unsigned int address) { return address / BLOCK_SIZE; }
 
-unsigned int _get_tag(unsigned int address) 
-{
+unsigned int _get_tag(unsigned int address) {
     unsigned int int_for_set_and_offset = pow(2, INDEX_SIZE + OFFSET_SIZE);
     return address / int_for_set_and_offset;
 }
 
-void _update_cache_count(bool data_is_in_cache) 
-{
-    if (data_is_in_cache) 
-    {
+void _update_cache_count(bool data_is_in_cache) {
+    if (data_is_in_cache) {
         cache.hit_count += 1;
     } else {
         cache.miss_count += 1;
